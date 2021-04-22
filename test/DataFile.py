2c76c9599d308c86abfb6b907853038e47b7b12f
Jakob Lass <lass.jakob@gmail.com>
2021-04-22 10:12:51 +0200

    Use os.path for cross platform compabilities

--- test/DataFile.py
+++ test/DataFile.py
@@ -1,5 +1,5 @@
 from DMCpy import DataFile
-
+import os.path
 
 def test_init():
     df = DataFile.DataFile()
@@ -13,20 +13,20 @@ def test_init():
     testDF = DataFile.DataFile('DEBUG')
     assert(testDF._debugging == True)
 
-    df = DataFile.DataFile(filePath=r'data\dmc2018n000401.hdf') 
-    path,name = os.path.split(r'data\dmc2018n000401.hdf')
+    df = DataFile.DataFile(filePath=os.path.join('data','dmc2018n000401.hdf'))
+    path,name = os.path.split(os.path.join('data','dmc2018n000401.hdf'))
 
     assert(df.folder == path)
     assert(df.fileName == name)
 
 
 def test_copy(): # Test the ability to copy from one data file to another
-    testDF = DataFile.DataFile('DEBUG')
+    testDF = DataFile.DataFile(os.path.join('data','dmc2018n000401.hdf'))
 
     testDFDict = testDF.__dict__
 
     dfCopy = DataFile.DataFile(testDF) # Perform copy
 
-    assert(dfCopy._debugging == True)
+    assert(dfCopy._debugging == False)
 
     assert(dfCopy==testDF)
--- test/DataSet.py
+++ test/DataSet.py
@@ -1,5 +1,6 @@
 from DMCpy import DataSet
 from DMCpy import DataFile
+import os.path
 
 def test_init():
     ds = DataSet.DataSet()
@@ -22,16 +23,16 @@ def test_init():
 def test_load():
 
     fileNumbers = range(401,411)
-    dataFiles = ['data/dmc2018n{:06d}.hdf'.format(no) for no in fileNumbers]
+    dataFiles = [os.path.join('data','dmc2018n{:06d}.hdf'.format(no)) for no in fileNumbers]
 
     ds = DataSet.DataSet(dataFiles)
     
     assert(len(ds) == len(dataFiles))
-    assert(ds[0].fileName == dataFiles[0].split('/')[-1])
+    assert(ds[0].fileName == os.path.split(dataFiles[0])[-1])
 
 
     # load single file and check that it is equal to the corresponding file in ds
     ds2 = DataSet.DataSet(dataFiles[-1])
     assert(len(ds2) == 1)
-    assert(ds2[0].fileName == dataFiles[-1].split('/')[-1])
+    assert(ds2[0].fileName == os.path.split(dataFiles[-1])[-1])
     assert(ds2[0] == ds[-1])
