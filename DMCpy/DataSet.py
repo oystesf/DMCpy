3a7ee9355fb2aa29045bfcd3c3267381ce038938
Jakob Lass <lass.jakob@gmail.com>
2021-04-23 11:23:59 +0200

    Create _getData method to update DataSet when data files change Split out generation of intensity as function of angle Add flag for vertical or correct summing Add documentation for plotTwoTheta

--- DMCpy/DataSet.py
+++ DMCpy/DataSet.py
@@ -33,9 +33,12 @@ class DataSet(object):
             except TypeError:
                 raise AttributeError('Provided dataFiles attribute is not itterable, filepath, or of type DataFile. Got {}'.format(dataFiles))
             
-            # Collect parameters listed below across data files into self
-            for parameter in ['counts','monitor','twoTheta','correctedTwoTheta','fileName','pixelPosition','waveLength']:
-                setattr(self,parameter,np.array([getattr(d,parameter) for d in self]))
+            self._getData()
+
+    def _getData(self):
+        # Collect parameters listed below across data files into self
+        for parameter in ['counts','monitor','twoTheta','correctedTwoTheta','fileName','pixelPosition','waveLength','mask']:
+            setattr(self,parameter,np.array([getattr(d,parameter) for d in self]))
 
 
     def __len__(self):
@@ -70,14 +73,26 @@ class DataSet(object):
 
     def next(self):
         return self.__next__()
-    
 
-    def plotTwoTheta(self,ax=None,thetaStart=None,thetaStop=None,dTheta=0.1,**kwargs):
-        """Plot intensity as function of correctedTwoTeha
 
+    def generateMask(self,maskingFunction = DataFile.maskFunction, **pars):
+        """Generate maks to applied to data in data file
+        
         Kwargs:
 
-            - ax (axis): Matplotlib axis into which data is plotted (default None - generates new)
+            - maskingFunction (function): Function called on self.phi to generate mask (default maskFunction)
+
+        All other arguments are passed to the masking function.
+
+        """
+        for d in self:
+            d.generateMask(maskingFunction,**pars)
+        self._getData()
+
+    def sumDetector(self,thetaStart=None,thetaStop=None,dTheta=0.1,corrected=True):
+        """Find intensity as function of either twoTheta or correctedTwoTheta
+
+        Kwargs:
 
             - thetaStart (float): Start of twoTheta plot in degrees (default minimum of all two thetas)
 
@@ -85,21 +100,31 @@ class DataSet(object):
 
             - dTheta (float): Step size in twoTheta in degrees (default 0.1)
 
-            - All other key word arguments are passed on to plotting routine
+            - corrected (bool): If true, use corrected two theta, otherwise sum vertically on detector (default True)
 
         Returns:
 
-            - ax: Matplotlib axis into which data was plotted
+            - twoTheta
+            
+            - Normalized Intensity
+            
+            - Normalized Intensity Error
 
         """
-        
+
+        if corrected:
+            twoTheta = self.correctedTwoTheta
+        else:
+            twoTheta = self.twoTheta
+
+
         if thetaStart is None:
-            anglesMin = np.min(self.correctedTwoTheta)
+            anglesMin = np.min(twoTheta)
         else:
             anglesMin = thetaStart
 
         if thetaStart is None:
-            anglesMax = np.max(self.correctedTwoTheta)
+            anglesMax = np.max(twoTheta)
         else:
             anglesMax = thetaStop
             
@@ -114,23 +139,53 @@ class DataSet(object):
 
         twoThetaBins = np.arange(anglesMin-0.5*dTheta,anglesMax+0.51*dTheta,dTheta)
 
-        summedRawIntenisty, _ = np.histogram(self.correctedTwoTheta.flatten(),bins=twoThetaBins,weights=self.counts.flatten())
-        summedIntensity, _ = np.histogram(self.correctedTwoTheta.flatten(),bins=twoThetaBins,weights=intensity.flatten())
-        summedMonitor, _ = np.histogram(self.correctedTwoTheta.flatten(),bins=twoThetaBins,weights=monitorRepeated.flatten())
+        summedRawIntenisty, _ = np.histogram(twoTheta[self.mask],bins=twoThetaBins,weights=self.counts[self.mask])
+        summedIntensity, _ = np.histogram(twoTheta[self.mask],bins=twoThetaBins,weights=intensity[self.mask])
+        summedMonitor, _ = np.histogram(twoTheta[self.mask],bins=twoThetaBins,weights=monitorRepeated[self.mask])
         
 
-        inserted, _  = np.histogram(self.correctedTwoTheta.flatten(),bins=twoThetaBins)
+        inserted, _  = np.histogram(twoTheta[self.mask],bins=twoThetaBins)
 
         normalizedIntensity = summedRawIntenisty/summedMonitor
         normalizedIntensityError =  np.sqrt(summedRawIntenisty)/summedMonitor
 
+        return twoThetaBins, normalizedIntensity, normalizedIntensityError
+    
+
+    def plotTwoTheta(self,ax=None,thetaStart=None,thetaStop=None,dTheta=0.1,corrected=True,**kwargs):
+        """Plot intensity as function of correctedTwoTheta or twoTheta
+
+        Kwargs:
+
+            - ax (axis): Matplotlib axis into which data is plotted (default None - generates new)
+
+            - thetaStart (float): Start of twoTheta plot in degrees (default minimum of all two thetas)
+
+            - thetaStop (float): Stop of twoTheta plot in degrees (default maximum of all two thetas)
+
+            - dTheta (float): Step size in twoTheta in degrees (default 0.1)
+
+            - corrected (bool): If true, use corrected two theta, otherwise sum vertically on detector (default True)
+
+            - All other key word arguments are passed on to plotting routine
+
+        Returns:
+
+            - ax: Matplotlib axis into which data was plotted
+
+        """
+        
+        
+        twoThetaBins, normalizedIntensity, normalizedIntensityError = self.sumDetector(thetaStart=thetaStart,thetaStop=thetaStop,dTheta=dTheta,corrected=corrected)
 
         TwoThetaPositions = 0.5*(twoThetaBins[:-1]+twoThetaBins[1:])
 
         if not 'fmt' in kwargs:
             kwargs['fmt'] = '-.'
 
-        fig,ax = plt.subplots()
+        if ax is None:
+            fig,ax = plt.subplots()
+
         ax.errorbar(TwoThetaPositions,normalizedIntensity,yerr=normalizedIntensityError,**kwargs)
         ax.set_xlabel(r'$2\theta$ [deg]')
         ax.set_ylabel(r'Intensity [arb]')
