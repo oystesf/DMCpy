b000fcdfa06ab2fd71e407be4f49a3fa2e4e93ff
Jakob Lass <lass.jakob@gmail.com>
2021-04-26 14:25:11 +0200

    Change saving location for calibration files

--- DMCpy/__init__.py
+++ DMCpy/__init__.py
@@ -5,6 +5,8 @@ import pickle
 __version__ = '0.1.4'
 __author__ = 'Jakob Lass'
 
-calibrationFile = os.path.join(os.path.split(os.path.abspath(__file__))[0],'data','calibrationDict.dat')
+
+
+calibrationFile = os.path.join(sys.prefix,'DMCpy','data','calibrationDict.dat')
 with open(calibrationFile,'rb') as f:
     calibrationDict = pickle.load(f)
