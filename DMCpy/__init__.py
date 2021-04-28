92ffe8f4353584694df8a62363e242f36224604d
Jakob Lass <lass.jakob@gmail.com>
2021-04-28 10:04:06 +0200

    Move data folder

--- DMCpy/__init__.py
+++ DMCpy/__init__.py
@@ -1,12 +1,12 @@
 import sys,os
 sys.path.append('.')
 import pickle
+import DMCpy
 
 __version__ = '0.1.4'
 __author__ = 'Jakob Lass'
 
-
-
-calibrationFile = os.path.join(sys.prefix,'DMCpy','data','calibrationDict.dat')
+installFolder = os.path.abspath(os.path.split(__file__)[0])
+calibrationFile = os.path.join(installFolder,'data','calibrationDict.dat')
 with open(calibrationFile,'rb') as f:
     calibrationDict = pickle.load(f)
