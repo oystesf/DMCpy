e45a8e8068a8f3c5f026492aac90ad3987156ee5
Jakob Lass <lass.jakob@gmail.com>
2021-04-28 09:37:19 +0200

    Update test to utilize os.path.join

--- test/_tools.py
+++ test/_tools.py
@@ -12,8 +12,7 @@ def test_fileListGenerator():
     year = 2021
     fileList = _tools.fileListGenerator(fileString,folder=folder,year=year)
 
-    expected = ['data\\dmc2021n000010.hdf', 'data\\dmc2021n000011.hdf', 'data\\dmc2021n000012.hdf', 'data\\dmc2021n000015.hdf', 'data\\dmc2021n000016.hdf', 'data\\dmc2021n000017.hdf', 'data\\dmc2021n000018.hdf', 'data\\dmc2021n000019.hdf', 'data\\dmc2021n000020.hdf', 'data\\dmc2021n000021.hdf', 'data\\dmc2021n000023.hdf']
-    
+    expected = [os.path.join('data',f) for f in ['dmc2021n000010.hdf', 'dmc2021n000011.hdf', 'dmc2021n000012.hdf', 'dmc2021n000015.hdf', 'dmc2021n000016.hdf', 'dmc2021n000017.hdf', 'dmc2021n000018.hdf', 'dmc2021n000019.hdf', 'dmc2021n000020.hdf', 'dmc2021n000021.hdf', 'dmc2021n000023.hdf']]  
     assert(np.all(expected==fileList))
 
     # Generate year and fileString containing data file numbers from fileList
