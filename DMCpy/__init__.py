4cc79b7b6fd47fb9b64428cfb386e98f8ff680f4
Jakob Lass <lass.jakob@gmail.com>
2021-05-04 11:18:50 +0200

    force wide scan for file

--- DMCpy/__init__.py
+++ DMCpy/__init__.py
@@ -15,17 +15,6 @@ try:
     with open(calibrationFile,'rb') as f:
         calibrationDict = pickle.load(f)
 except FileNotFoundError:
-    print('Current folder is '+__file__)
-    print("Contents of local folder is: {}".format(os.listdir(installFolder)))
-    print('Content of parent folder is: {}'.format(os.listdir(os.path.join(installFolder,'..'))))
-    print('Content of parent folder is: {}'.format(os.listdir(os.path.join(installFolder,'..','..'))))
-    print('Content of parent folder is: {}'.format(os.listdir(os.path.join(installFolder,'..','..','..'))))
-    print('Content of parent folder is: {}'.format(os.listdir(os.path.join(installFolder,'..','..','..','..'))))
-    print('Content of parent folder is: {}'.format(os.listdir(os.path.join(installFolder,'..','..','..','..','..'))))
-    print('Content of parent folder is: {}'.format(os.listdir(os.path.join(installFolder,'..','..','..','..','..','..'))))
-
-    
-
     def find(name, path):
         result = []
         for root, dirs, files in os.walk(path):
@@ -33,5 +22,9 @@ except FileNotFoundError:
                 result.append(os.path.join(root, name))
         return result
 
-    print(find('calibrationDict.dat',os.path.abspath(os.path.join(installFolder,'..','..','..','..','..','..'))))
-    raise FileNotFoundError
+    
+    foundFile = find('calibrationDict.dat',os.path.abspath(os.path.join(installFolder,'..','..','..','..','..','..')))
+
+    with open(foundFile,'rb') as f:
+        calibrationDict = pickle.load(f)
+    #raise FileNotFoundError
