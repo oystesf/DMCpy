08be437d48af9f6d39bf4a25effea94f823f64ef
Jakob Lass <lass.jakob@gmail.com>
2021-12-21 13:33:14 +0100

    Update version

--- DMCpy/__init__.py
+++ DMCpy/__init__.py
@@ -2,7 +2,7 @@ import sys,os
 sys.path.append('.')
 import pickle,numpy as np
 
-__version__ = '0.1.7'
+__version__ = '0.1.10'
 __author__ = 'Jakob Lass'
 
 # installFolder = os.path.abspath(os.path.split(__file__)[0])
