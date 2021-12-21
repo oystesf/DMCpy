ff545b08edcd89bb7eb3456a0dfdcde0b4adb1cc
Jakob Lass <lass.jakob@gmail.com>
2021-12-21 12:51:48 +0100

    Add more files to git add call

--- make.py
+++ make.py
@@ -123,6 +123,8 @@ elif args.task.lower() == 'version':
                 os.path.join('docs','Tutorials','*'),
                 os.path.join('docs','index.rst'),
                 os.path.join('DMCpy','__init__.py'),
+                os.path.join('test','init.py'),
+                'setup.py',
                 ]
     os.system("git add {}".format(' '.join(addFiles)))
     os.system("git commit -m 'Update version'")
