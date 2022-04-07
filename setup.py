ac86aebea510702fa8c5d0f622b6e51e04ad5bc9
Jakob Lass <lass.jakob@gmail.com>
2022-04-07 13:39:56 +0200

    Update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ pythonPath =  os.path.relpath(os.path.join(installFolder,'DMCpy'),sys.base_prefi
 
 setup(
     name='DMCpy',
-    version='0.2.0',
+    version='0.2.1',
     description=('Python software packaged designed for reduction of neutron powder diffraction data from DMC at PSI.'),
     long_description=long_description,
     author='Jakob Lass',
