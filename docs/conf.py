27da2cf3f0e1883d229d16e57278aeedbf177a8f
Oystein Slagtern Fjellvag <oystein.fjellva@live.no>
2023-05-25 08:48:56 +0200

    Update version

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,9 @@ copyright = u'2023, Jakob Lass & Øystein Slagtern Fjellvåg'
 author = u'Jakob Lass'
 
 # The short X.Y version
-version = u'0.2.5'
+version = u'version=0.9.0'
 # The full version, including alpha/beta/rc tags
-release = u'0.2.5'
+release = u'version=0.9.0'
 
 
 
